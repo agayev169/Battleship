5832f1e8164ca6ef195ba12af733f58b8c31b37b
agayev169 <agayev169@gmail.com>
2019-03-07 12:58:14 +0400

    Complete Ship.java

--- src/Ship.java
+++ src/Ship.java
@@ -1,5 +1,4 @@
 public class Ship {
-    private int segmentNum;
     private int x;
     private int y;
     private boolean isHorizontal;
@@ -8,7 +7,24 @@ public class Ship {
     public Ship(int x, int y, int segmentNum, boolean isHorizontal) {
         this.x = x;
         this.y = y;
-        this.segmentNum = segmentNum;
         this.isHorizontal = isHorizontal;
+        health = new boolean[segmentNum];
+        for (int i = 0; i < segmentNum; ++i) {
+            health[i] = true;
+        }
+    }
+
+    public void getDamage(int x, int y) {
+        if ((this.x - x != 0 && this.y - y != 0) ||
+                (this.x - x < 0 || this.x - x > health.length ||
+                        this.y - y < 0 || this.y - y > health.length)) {
+            System.out.println("Error in getDamage function. Wrong coordinates for the ship.");
+            return;
+        }
+        if (x < 0 || x > 10 || y < 0 || y > 10) {
+            System.out.println("Error in getDamage function. Wrong coordinates (negative value) for the ship.");
+            return;
+        }
+        health[this.x - x + this.y - y] = false;
     }
 }
