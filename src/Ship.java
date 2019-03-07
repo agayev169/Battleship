4a3355dfa4561972fdc1875fbf6cbed8fc6a32a5
agayev169 <agayev169@gmail.com>
2019-03-07 13:17:26 +0400

    Add toString and isDead methods and test Ship

--- src/Ship.java
+++ src/Ship.java
@@ -15,9 +15,10 @@ public class Ship {
     }
 
     public void getDamage(int x, int y) {
-        if ((this.x - x != 0 && this.y - y != 0) ||
-                (this.x - x < 0 || this.x - x > health.length ||
-                        this.y - y < 0 || this.y - y > health.length)) {
+        if ((x - this.x != 0 && y - this.y != 0) ||
+                x - this.x < 0 || x - this.x > health.length ||
+                        y - this.y < 0 || y - this.y > health.length ||
+                            (isHorizontal && y - this.y != 0) || (!isHorizontal && x - this.x != 0)) {
             System.out.println("Error in getDamage function. Wrong coordinates for the ship.");
             return;
         }
@@ -25,6 +26,39 @@ public class Ship {
             System.out.println("Error in getDamage function. Wrong coordinates (negative value) for the ship.");
             return;
         }
-        health[this.x - x + this.y - y] = false;
+        health[(isHorizontal) ? x - this.x : y - this.y] = false;
+    }
+
+    public boolean isDead() {
+        for (int i = 0; i < health.length; ++i) {
+            if (health[i]) return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        String result = "x: " + x + ", y: " + y + ", orientation: ";
+        result += (isHorizontal) ? 'H' : 'V';
+        result += ", health: ";
+        for (int i = 0; i < health.length; ++i) {
+            result += (health[i]) ? "o " : "x ";
+        }
+        if (isDead()) result += "\b, is dead";
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Ship ship = new Ship(0, 0, 4, false);
+        ship.getDamage(0, 0);
+        System.out.println(ship);
+        ship.getDamage(0, 1);
+        System.out.println(ship);
+        ship.getDamage(2, 0);
+        System.out.println(ship);
+        ship.getDamage(3, 0);
+        System.out.println(ship);
+        ship.getDamage(3, 1);
+        System.out.println(ship);
     }
 }
