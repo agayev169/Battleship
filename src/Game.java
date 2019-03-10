11c4c6158608bb2b85eb0200792cc45192e69c2d
agayev169 <agayev169@gmail.com>
2019-03-10 17:02:56 +0400

    Fix canBuild function

--- src/Game.java
+++ src/Game.java
@@ -45,7 +45,7 @@ public class Game {
 
     public boolean canBuild(int x, int y, int segmentNum, boolean isHorizontal, boolean isUser) {
         boolean[][] hasShip = new boolean[10][10];
-        if ((isHorizontal && (x + segmentNum >= 10 || x <= 0)) || (!isHorizontal && (y + segmentNum >= 10 || y <= 0)))
+        if ((isHorizontal && (x + segmentNum >= 10 || x < 0)) || (!isHorizontal && (y + segmentNum >= 10 || y < 0)))
             return  false;
         if (isUser) {
             for (Ship ship : ships1) {
