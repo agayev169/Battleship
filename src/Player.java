4fa458346e931ebf3630ff3f7b910700dedf556f
agayev169 <agayev169@gmail.com>
2019-03-14 08:23:10 +0400

    Make Player an abstract class

--- src/Player.java
+++ src/Player.java
@@ -1,4 +1,13 @@
-public interface Player {
-    void buildShips();
-    void attack();
+import java.io.IOException;
+
+public abstract class Player {
+    private Ship[] ships = new Ship[5];
+    Game game;
+
+    public Player(Game game) {
+        this.game = game;
+    }
+
+    public abstract void buildShips() throws IOException;
+    public abstract void attack();
 }
