06c89ed83825e7a7a53b607b6d23f3c85eff1dce
agayev169 <agayev169@gmail.com>
2019-03-16 01:28:54 +0400

    Add new tests

--- src/TestClass.java
+++ src/TestClass.java
@@ -1,4 +1,6 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
@@ -28,5 +30,47 @@ public class TestClass {
             assertFalse(game.attemptToBuild(x, -1, 2, false, 0));
             assertFalse(game.attemptToBuild(x, 10, 2, false, 0));
         }
+    }
+
+    @Test
+    void canBuildInsideInside() {
+        Game game = new Game();
+        for (int y = 0; y < 10; ++y) {
+            for (int x = 0; x < 9; ++x) {
+                assertTrue(game.canBuild(x, y, 2, true, 1));
+            }
+        }
+    }
+
+    @Test
+    void attemptBuildInsideMoreThanFive() {
+        Game game = new Game();
+        assertTrue(game.attemptToBuild(0, 0, 2, true, 1));
+        assertTrue(game.attemptToBuild(2, 0, 2, true, 1));
+        assertTrue(game.attemptToBuild(4, 0, 2, true, 1));
+        assertTrue(game.attemptToBuild(6, 0, 2, true, 1));
+        assertTrue(game.attemptToBuild(8, 0, 2, true, 1));
+        assertFalse(game.attemptToBuild(0, 5, 2, true, 1));
+    }
+
+    @Test
+    void attemptBuildOverShip() {
+        Game game = new Game();
+        assertTrue(game.attemptToBuild(0, 0, 2, true, 1));
+        assertFalse(game.attemptToBuild(0, 0, 2, true, 1));
+        assertFalse(game.attemptToBuild(1, 0, 2, true, 1));
+
+        assertTrue(game.attemptToBuild(5, 5, 2, false, 1));
+        assertFalse(game.attemptToBuild(5, 5, 2, false, 1));
+        assertFalse(game.attemptToBuild(5, 6, 2, false, 1));
+    }
+
+    @Test
+    void shoot() {
+        Game game = new Game();
+        assertTrue(game.attemptToBuild(0, 0, 2, true, 1));
+        assertEquals(game.shoot(0, 0, 2), 1);
+        assertEquals(game.shoot(1, 0, 2), 2);
+        assertEquals(game.shoot(5, 5, 2), 0);
     }
 }
