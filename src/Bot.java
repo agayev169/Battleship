57766ec809974c64490c7fb71711568bd62dd400
agayev169 <agayev169@gmail.com>
2019-03-17 18:09:32 +0400

    Fix the bug with building and attacking ships outside of the field

--- src/Bot.java
+++ src/Bot.java
@@ -13,8 +13,8 @@ public class Bot extends Player {
         int[] shipSize = new int[] {2, 3, 3, 4, 5};
         int i = 0;
         while (i < shipSize.length) {
-            int x = rand.nextInt(11);
-            int y = rand.nextInt(11);
+            int x = rand.nextInt(10);
+            int y = rand.nextInt(10);
             boolean isHorizontal = rand.nextBoolean();
             if (attemptToBuild(x, y, shipSize[i], isHorizontal)) {
                 ++i;
@@ -28,8 +28,8 @@ public class Bot extends Player {
 
         int retVal = HIT;
         while (retVal != MISS) {
-            int x = rand.nextInt(11);
-            int y = rand.nextInt(11);
+            int x = rand.nextInt(10);
+            int y = rand.nextInt(10);
             retVal = getGame().shoot(x, y, getId());
         }
     }
