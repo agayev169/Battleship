b23de86035aaafe687431cadbb44ed1702a153b8
agayev169 <agayev169@gmail.com>
2019-04-24 11:23:42 +0400

    Add GameServer class

---
+++ src/GameServer.java
@@ -0,0 +1,51 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class GameServer {
+    private final ServerSocket serverSocket;
+    private final Socket socket;
+    private final Game game;
+
+    private BufferedReader reader;
+    private PrintWriter writer;
+
+    public GameServer(Game game) throws IOException {
+        this.game = game;
+        serverSocket = new ServerSocket(1337);
+        socket = serverSocket.accept();
+
+        reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        writer = new PrintWriter(socket.getOutputStream());
+    }
+
+    public ServerSocket getServerSocket() {
+        return serverSocket;
+    }
+
+    public Socket getSocket() {
+        return socket;
+    }
+
+    public Game getGame() {
+        return game;
+    }
+
+    public String read() throws IOException {
+        return reader.readLine();
+    }
+
+    public void write(String toWrite) {
+        writer.write(toWrite);
+    }
+
+    public void close() throws IOException {
+        socket.close();
+        serverSocket.close();
+        reader.close();
+        writer.close();
+    }
+}
