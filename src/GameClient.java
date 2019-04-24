167f4d02800d96fbb3163463266e0028cca3954f
agayev169 <agayev169@gmail.com>
2019-04-24 11:27:07 +0400

    Add GameClient class

---
+++ src/GameClient.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.Socket;
+
+public class GameClient {
+    private final Socket socket;
+    private final Game game;
+
+    private BufferedReader reader;
+    private PrintWriter writer;
+
+    public GameClient(Game game) throws IOException {
+        this.game = game;
+        socket = new Socket(InetAddress.getLocalHost(), 1337);
+
+        reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        writer = new PrintWriter(socket.getOutputStream());
+    }
+
+    public String read() throws IOException {
+        return reader.readLine();
+    }
+
+    public void write(String toWrite) {
+        writer.write(toWrite);
+    }
+
+    public void close() throws IOException {
+        socket.close();
+        reader.close();
+        writer.close();
+    }
+}
