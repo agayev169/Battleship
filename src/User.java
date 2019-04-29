b5a0bbf75a56ecf1d461b00d04ab4aec33de5b3e
agayev169 <agayev169@gmail.com>
2019-04-30 00:36:48 +0400

    Fix error occuring in the case of misstyping a letter instead of a number during building and attacking in terminal mode

--- src/User.java
+++ src/User.java
@@ -1,4 +1,5 @@
 import java.io.IOException;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class User extends Player {
@@ -17,10 +18,12 @@ public class User extends Player {
                 (getGame().getNetworkManager().getClass().getName().equals("GameClient") && getId() == 0) ||
                         (getGame().getNetworkManager().getClass().getName().equals("GameServer") && getId() == 1))) {
             System.out.println("Waiting for opponent");
-            getGame().getGridPanel().getListener().setActive(false);
+            if (getGame().getUserInterface() == Game.GUI)
+                getGame().getGridPanel().getListener().setActive(false);
 //            gridPanel.setReady(false);
             String move = getGame().getNetworkManager().read();
-            getGame().getGridPanel().getListener().setActive(true);
+            if (getGame().getUserInterface() == Game.GUI)
+                getGame().getGridPanel().getListener().setActive(true);
             String[] lines = move.split(System.getProperty("line.separator"));
             for (String line : lines) {
                 String[] args = line.split(" ");
@@ -32,8 +35,6 @@ public class User extends Player {
         int lineCounter = 0;
         lastMove = new StringBuilder();
         if (userInterface == Game.TERMINAL) {
-            Scanner sc = new Scanner(System.in);
-
             int[] shipCount = new int[4];
             shipCount[0] = 1;
             shipCount[1] = 2;
@@ -41,41 +42,48 @@ public class User extends Player {
             shipCount[3] = 1;
             boolean lastAttempt = true;
             while (shipCount[0] > 0 || shipCount[1] > 0 || shipCount[2] > 0 || shipCount[3] > 0) {
-                showGridMine(true);
-                if (!lastAttempt) {
-                    System.out.println("Cannot build a ship with this configuration. Try again.");
-                }
-                int x, y, segmentNum;
-                boolean isHorizontal;
-                System.out.print("Which type of ship do you want to place? (");
-                for (int i = 0; i < shipCount.length; ++i) {
-                    for (int j = 0; j < shipCount[i]; ++j) {
-                        System.out.print((i + 2) + ", ");
+                try {
+                    Scanner sc = new Scanner(System.in);
+
+                    showGridMine(true);
+                    if (!lastAttempt) {
+                        System.out.println("Cannot build a ship with this configuration. Try again.");
                     }
-                }
-                System.out.print("\b\b) ");
-                segmentNum = sc.nextInt();
-                if (segmentNum < 2 || segmentNum > 5 || shipCount[segmentNum - 2] <= 0) {
-                    lastAttempt = false;
-                    continue;
-                }
-                System.out.print("Where do you want to place a ship? (Ex: b 5) ");
-                char letter = sc.next().charAt(0);
-                x = letter - 'a';
-                y = sc.nextInt() - 1;
-                char orientation;
-                do {
-                    System.out.print("Orientation (V/H): ");
-                    orientation = sc.next().charAt(0);
-                    isHorizontal = (orientation == 'H');
-                    if (orientation != 'H' && orientation != 'V') System.out.println("Wrong orientation. Try again.");
-                } while (orientation != 'H' && orientation != 'V');
-                lastMove.append(x + " " + y + " " + segmentNum + " " + orientation + "\n");
-                ++lineCounter;
-                if (attemptToBuild(x, y, segmentNum, isHorizontal)) {
-                    --shipCount[segmentNum - 2];
-                    lastAttempt = true;
-                } else {
+                    int x, y, segmentNum;
+                    boolean isHorizontal;
+                    System.out.print("Which type of ship do you want to place? (");
+                    for (int i = 0; i < shipCount.length; ++i) {
+                        for (int j = 0; j < shipCount[i]; ++j) {
+                            System.out.print((i + 2) + ", ");
+                        }
+                    }
+                    System.out.print("\b\b) ");
+                    segmentNum = sc.nextInt();
+                    if (segmentNum < 2 || segmentNum > 5 || shipCount[segmentNum - 2] <= 0) {
+                        lastAttempt = false;
+                        continue;
+                    }
+                    System.out.print("Where do you want to place a ship? (Ex: b 5) ");
+                    char letter = sc.next().charAt(0);
+                    x = letter - 'a';
+                    y = sc.nextInt() - 1;
+                    char orientation;
+                    do {
+                        System.out.print("Orientation (V/H): ");
+                        orientation = sc.next().charAt(0);
+                        isHorizontal = (orientation == 'H');
+                        if (orientation != 'H' && orientation != 'V')
+                            System.out.println("Wrong orientation. Try again.");
+                    } while (orientation != 'H' && orientation != 'V');
+                    lastMove.append(x + " " + y + " " + segmentNum + " " + orientation + "\n");
+                    ++lineCounter;
+                    if (attemptToBuild(x, y, segmentNum, isHorizontal)) {
+                        --shipCount[segmentNum - 2];
+                        lastAttempt = true;
+                    } else {
+                        lastAttempt = false;
+                    }
+                } catch (InputMismatchException e) {
                     lastAttempt = false;
                 }
             }
@@ -95,10 +103,12 @@ public class User extends Player {
                 (getGame().getNetworkManager().getClass().getName().equals("GameClient") && getId() == 0) ||
                         (getGame().getNetworkManager().getClass().getName().equals("GameServer") && getId() == 1))) {
             System.out.println("Waiting for opponent");
-            getGame().getGridPanel().getListener().setActive(false);
+            if (getGame().getUserInterface() == Game.GUI)
+                getGame().getGridPanel().getListener().setActive(false);
 //            gridPanel.setReady(false);
             String move = getGame().getNetworkManager().read();
-            getGame().getGridPanel().getListener().setActive(true);
+            if (getGame().getUserInterface() == Game.GUI)
+                getGame().getGridPanel().getListener().setActive(true);
             String[] lines = move.split(System.getProperty("line.separator"));
             for (String line : lines) {
                 String[] args = line.split(" ");
